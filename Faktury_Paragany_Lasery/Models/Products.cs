78d2637df76fce88dadac7ad19a95e86eacadbfe
Marceli-1 <56529425+Marceli-1@users.noreply.github.com>
2021-06-18 15:45:48 +0200

    ViewModel, Model Products backbone

---
+++ Faktury_Paragany_Lasery/Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktury_Paragany_Lasery.Models
+{
+    public class Products
+    {
+        public string name { get; set; }
+        public float volume { get; set; }
+
+        public Products(string name, float volume)
+        {
+            this.name = name;
+            this.volume = volume;
+        }
+
+        public Products()
+        {
+        }
+    }
+}
---
+++ Faktury_Paragany_Lasery/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Faktury_Paragany_Lasery.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktury_Paragany_Lasery.ViewModel
+{
+    using Models;
+    using Commands;
+    using System.Runtime.CompilerServices;
+
+    public class ProductsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged; 
+
+        public ObservableCollection<Products> CurrentProduct
+        {
+            get
+            {
+                return new ObservableCollection<Products>(){new Products() { name = "cukierek", volume = 12 },
+                    new Products() { name = "Auto", volume = 1 } };
+            }
+        }
+
+        public void SetPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ProductsViewModel() { }
+    }
+}
+
